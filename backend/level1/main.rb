4a4f849d898eaecd18ded6188e50efde4d772cda
amelielescure <amelieles78@gmail.com>
2016-08-18 22:48:01 +0200

    Add serializer RentalCar to create output object and return hash

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,23 +1,25 @@
 require 'json'
 require 'virtus'
 require 'date'
+require 'active_model'
 
 class Drivy
   def initialize
     data = read_json('data.json')
-    output_hash = {}
-    output_hash['rentals'] = []
 
     cars = data['cars']
     rentals = data['rentals']
     
-    rentals.each do |rental_json|
-      rental_json['car'] = cars.select { |car| car['id'] == rental_json['car_id'] }.first
+    rental_cars = rentals.map do |rental_json|
+      rental_json['car'] = data['cars'].select { |car| car['id'] == rental_json['car_id'] }.first
       rental = Rental.new(rental_json)
-      output_hash['rentals'] << { id: rental.id, price: rental.amount }
+      RentalCar.new(id: rental.id, price: rental.amount)
     end
 
-    generate_json(output_hash)
+    output = {}
+    output['rentals'] = rental_cars.as_json
+
+    generate_json(output)
   end
 
   def read_json file
@@ -30,6 +32,14 @@ class Drivy
       f.write( JSON.pretty_generate(data) )
     end
   end
+end
+
+class RentalCar
+  include ActiveModel::Serializers::JSON
+  include Virtus.model
+
+  attribute :id, Integer
+  attribute :price, Integer
 end
 
 class Car
@@ -52,11 +62,11 @@ class Rental
   attribute :car, Car
 
   def duration
-    self.end_date - self.start_date.prev_day(1)
+    end_date - start_date.prev_day(1)
   end
 
   def amount
-    self.duration.to_i * self.car.price_per_day + self.distance * self.car.price_per_km
+    duration.to_i * car.price_per_day + distance * car.price_per_km
   end
 end
 
