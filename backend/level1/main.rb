7319f415d7e905e743130796723dbdf1aa0b34ba
amelielescure <amelieles78@gmail.com>
2016-08-18 20:08:12 +0200

    Level 1 - calcul amount of rental car initialisation

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,63 @@
-require "json"
+require 'json'
+require 'virtus'
+require 'date'
 
-# your code
+class Drivy
+  def initialize
+    data = read_json('data.json')
+    output_hash = {}
+    output_hash['rentals'] = []
+
+    cars = data['cars']
+    rentals = data['rentals']
+    
+    rentals.each do |rental_json|
+      rental_json['car'] = cars.select { |car| car['id'] == rental_json['car_id'] }.first
+      rental = Rental.new(rental_json)
+      output_hash['rentals'] << { id: rental.id, price: rental.amount }
+    end
+
+    generate_json(output_hash)
+  end
+
+  def read_json file
+    file = File.read(file)
+    JSON.parse(file)
+  end
+
+  def generate_json data
+    File.open('myoutput.json', 'w') do |f|
+      f.write( JSON.pretty_generate(data) )
+    end
+  end
+end
+
+class Car
+  include Virtus.model
+
+  attribute :id, Integer
+  attribute :price_per_day, Integer
+  attribute :price_per_km, Integer
+end
+
+class Rental
+  include Virtus.model
+
+  attribute :id, Integer
+  attribute :car_id, Integer
+  attribute :start_date, DateTime
+  attribute :end_date, DateTime
+  attribute :distance, Integer
+
+  attribute :car, Car
+
+  def duration
+    self.end_date - self.start_date.prev_day(1)
+  end
+
+  def amount
+    self.duration.to_i * self.car.price_per_day + self.distance * self.car.price_per_km
+  end
+end
+
+Drivy.new
