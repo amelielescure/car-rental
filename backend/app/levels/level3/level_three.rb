5df3f57d482f89235a43f1febdd6e89ffc672f62
amelielescure <amelieles78@gmail.com>
2016-08-20 21:31:47 +0200

    Refacto level 3 to validate car and rental

--- backend/app/levels/level3/level_three.rb
+++ backend/app/levels/level3/level_three.rb
@@ -6,16 +6,18 @@ class LevelThree
     rentals = data['rentals']
     
     rental_cars = rentals.map do |rental_json|
-      rental_json['car'] = data['cars'].select { |car| car['id'] == rental_json['car_id'] }.first
       rental = RentalDeserializer.new(rental_json)
-      if rental.valid?
+      rental.car = CarDeserializer.new( data['cars'].select { |car| car['id'] == rental_json['car_id'] }.first )
+
+      if rental.valid? && rental.car.valid?
         commission = Commission.new(
           insurance_fee: rental.insurance_fee ,
           assistance_fee: rental.assistance_fee ,
           drivy_fee: rental.drivy_fee
         )
+
         RentalPrice.new(
-          id: rental.id, 
+          id: rental.id,
           price: rental.discount_price, commission: commission
         ).as_json(only: [:id, :price, :commission])
       end
