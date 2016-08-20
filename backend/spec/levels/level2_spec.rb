c64d955febfaa5e676f659dcaa4c951580f7c6ff
amelielescure <amelieles78@gmail.com>
2016-08-20 19:31:52 +0200

    Refactor duplicated code in spec

--- backend/spec/levels/level2_spec.rb
+++ backend/spec/levels/level2_spec.rb
@@ -6,21 +6,6 @@ describe 'Level 2' do
 			LevelTwo.new
 		end
 
-		context 'with a valid data source' do
-			let(:expected_data) { JsonFileHelper.read_json('app/levels/level2/output.json') }
-			let(:generated_data) { JsonFileHelper.read_json('app/levels/level2/myoutput.json') }
-
-			it 'generates data output not be nil' do
-				expect(generated_data).not_to be_nil
-			end
-
-			it 'generates a level file with items' do
-				expect(generated_data).not_to be_empty
-			end
-
-			it 'generates an identical file' do
-				expect(generated_data).to eq expected_data
-			end
-		end
+		include_examples 'Levels', 'app/levels/level2'
 	end
 end
--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../../app", __FILE__)
 require 'main'
 
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
+
 RSpec.configure do |config|
 end
